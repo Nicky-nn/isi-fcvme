dcec885e7c27a82028c3596574fe1581a3140407
Nick Russell <nickynn07@gmail.com>
2023-08-09 15:12:11 -0400

    first commit

---
+++ src/app/modules/productos/services/ProductoComposeService.ts
@@ -0,0 +1,25 @@
+import { genReplaceEmpty } from '../../../utils/helper'
+import { ProductoInputProps } from '../interfaces/producto.interface'
+
+/**
+ * Componemos el producto para su posterior guardado
+ * @param prod
+ */
+
+export const productoComposeService = (aql: ProductoInputProps): any => {
+  return {
+    // codigoProducto: aql.codigoProducto,
+    nombre: aql.nombre,
+    descripcion: aql.descripcion,
+    descripcionHtml: aql.descripcionHtml,
+    codigoActividad: aql.codigoActividad?.codigoActividad,
+    codigoProductoSin: aql.codigoProductoSin?.codigoProducto,
+    precio: aql.precio,
+    codigoUnidadMedida: parseInt(
+      genReplaceEmpty(aql.codigoUnidadMedida?.codigoClasificador, 0),
+      10,
+    ),
+    tipoProductoId: aql.tipoProducto?._id,
+    codigoProveedor: aql.codigoProveedor?.codigo,
+  }
+}
